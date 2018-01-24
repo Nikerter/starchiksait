5b514588d567dc261fdd8cd2b965be351a104855
Nikerter <nekit9607@mail.ru>
2018-01-25 02:30:39 +0300

    Бизнес проверка на почту (не проверено)

---
+++ scripts/business.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<?php
+   $back = "<p><a href=\"javascript: history.back()\">Вернуться назад</a></p>";
+ 
+   if(!empty($_POST['label']) and !empty($_POST['adres']) and !empty($_POST['tel']) 
+   and !empty($_POST['mail'])){
+      $label = trim(strip_tags($_POST['label']));
+      $adres = trim(strip_tags($_POST['adres']));
+      $tel = trim(strip_tags($_POST['tel']));
+      $mail = trim(strip_tags($_POST['mail']));
+ 
+      mail('nekit@gmail.com', 'Письмо с адрес_вашего_сайта', 
+      'Вам написал: '.$label.'<br />Его номер: '.$phone.'<br />Его почта: '.$mail.'<br />
+      Его сообщение: '.$message,"Content-type:text/html;charset=windows-1251");
+ 
+      echo "Ваше сообщение успешно отправлено!<Br> Вы получите ответ в 
+      ближайшее время<Br> $back";
+ 
+      exit;
+   } 
+   else {
+      echo "Для отправки сообщения заполните все поля! $back";
+      exit;
+   }
+?>
+
+</body>
+</html>
+
